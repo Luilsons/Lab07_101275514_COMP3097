62e664addd5c43cea95226c708785191822b86e5
Luilson Sousa <luilsonms@hotmail.com>
2025-02-19 18:49:21 -0500

    Initial Commit

---
+++ Lab07TimeTableApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab07TimeTableApp
+//
+//  Created by Luilson Marcos Sousa dos Reis on 2025-02-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lab07TimeTableAppUITests/Lab07TimeTableAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lab07TimeTableAppUITestsLaunchTests.swift
+//  Lab07TimeTableAppUITests
+//
+//  Created by Luilson Marcos Sousa dos Reis on 2025-02-19.
+//
+
+import XCTest
+
+final class Lab07TimeTableAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
